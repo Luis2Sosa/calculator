209bdae64a62aeba50fb2e9cb5de1c779bd04a2f
Luis Sosa <luismedina2320@hotmail.com>
2025-05-07 17:36:18 -0400

    Añadi comentarios explicativios en el proyecto calculator

--- calculator.py
+++ calculator.py
@@ -1,18 +1,23 @@
+# Importamos los módulos
 import random
-
 import math
 
+# Damos la bienvenida al usuario
 print("Bienvendio a la calculadora aleatoria\n")
+
+# Un numero aleatorio del 10 al 100
 r1 = random.randint(10, 100)
-print(f"El numero aleatorio para la operacion es: {r1}")
+print(f"El numero aleatorio para la operacion es: {r1}\n")
+
 
 print("Raiz")
 print("Cuadrado")
 print("Ambos")
 
+# Pedimos al usuario que introduzca una opción de la que hay disponible
+opcion =  input("Introduzca la opcion de la anteriores que desea operar con el numero aleatorio:\n").lower()
 
-opcion =  input("Introduzca la opcion que desea operar con el numero aleatorio:\n").lower()
-
+# Usamos las condiciones para cada caso
 if opcion == "raiz":
     print(f"La raiz cuadrada con el numero aleatorio es {math.sqrt(r1):.2}")
 elif opcion == "cuadrado":
